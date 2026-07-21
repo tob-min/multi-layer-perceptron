06c5f2a475475710fd29361ebe4be09041af73fe
Toby Minor <189789634+tob-min@users.noreply.github.com>
2026-07-21 21:34:14 +0100

    Removed functionality from bloated node class

--- src/node.py
+++ src/node.py
@@ -1,35 +1,32 @@
 import numpy as np
 from typing import Callable
 
-class Perceptron_Node:
+class Node:
 
-    def __init__(self, input_count: int, weights: np.ndarray[tuple[int], np.dtype[np.float64]], activation_function: Callable[[np.float64], np.float64]) -> None:
-        """Initialize a node with weights and an activation function.
+    activation: Callable
+    activation_prime: Callable
+    weighted_sum: float
+
+    def __init__(self, activation_function: Callable[[float], float], activation_prime: Callable[[float], float]) -> None:
+        """Initialize a node with an activation function.
 
         Args:
-            input_count: Number of expected input values.
-            weights: Weight vector including bias at index 0. Length must equal input_count + 1.
             activation_function: Activation function applied to the node's weighted sum.
+            activation_prime: Derivative of activation function
 
         Raises:
             ValueError: If len(weights) != input_count + 1.
         """
         
-        self.input_count = input_count
-        # size of w must be equal to the number of inputs (plus one for bias)
-        if len(weights) != input_count + 1:
-            raise ValueError("Length of weights must be one more than the number of inputs")
-        self.weights = weights
-        self.phi = activation_function
+        self.activation = activation_function
+        self.activation_prime = activation_prime
 
-    def set_weights(self, weights: np.ndarray) -> None:
-        self.weights = weights
-
-    def calculate(self, inputs: np.ndarray[tuple[int], np.dtype[np.float64]]) -> np.float64:
-        """Compute the node output for a given input vector.
+    def calculate(self, inputs: np.ndarray, input_weights: np.ndarray) -> float:
+        """Compute the node output for a given input vector and store weighted sum.
 
         Args:
-            inputs: Input array of shape (input_count,).
+            inputs: Input array.
+            input_weights: Weight array of length len(inputs) + 1.
 
         Returns:
             Activated scalar output.
@@ -38,8 +35,20 @@ class Perceptron_Node:
             ValueError: If len(inputs) != input_count.
         """
 
-        if len(inputs) != self.input_count:
-            raise ValueError("Number of inputs must equal node's input count")
+        if len(input_weights) != len(inputs) + 1:
+            raise ValueError("Weight array must be of length one greater than input array")
         
-        a = self.weights[0] + np.dot(inputs, self.weights[1:])
-        return self.phi(a)
+        self.weighted_sum = input_weights[0] + np.dot(inputs, input_weights[1:])
+        return self.activation(self.weighted_sum)
+
+    def calc_gradient(self, output_weights: np.ndarray, output_dels: np.ndarray) -> float:
+        """Compute del E/del a for E the overall error and a the weighted sum in this node
+        
+        Args: 
+            output_dels: Input array of del E/ del ak for the weighed sum ak of each output node k.
+            output_weights: Output weight array of same dimensions as output_dels
+
+        Returns:
+            Calculated gradient
+        """
+        return self.activation_prime(self.weighted_sum) * np.dot(output_weights, output_dels)
