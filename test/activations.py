78d870d0d57742d8211706570b7f81f31756a59c
Toby Minor <189789634+tob-min@users.noreply.github.com>
2026-07-26 16:48:29 +0100

    Moved activation functions to own  file

---
+++ test/activations.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def mean_squared_error_derivative(y: float, y_target: float) -> float:
+    """Return the derivative of the mean squared error with respect to the prediction."""
+    return 2 * (y - y_target)
+
+def sigmoid(x: float) -> float:
+    """Apply the sigmoid activation function to a scalar value."""
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x: float) -> float:
+    """Return the derivative of the sigmoid activation function."""
+    sigma_x = sigmoid(x)
+    return sigma_x * (1 - sigma_x)
+
+def tanh(x: float) -> float:
+    return np.tanh(x)
+
+def tanh_derivative(x: float) -> float:
+    return 1 - np.tanh(x) ** 2
